10d920c584899f73530cb48940d3e2fb7f27bdfa
LeeDongKyu <m3088787@naver.com>
2023-07-21 12:52:58 +0900

    [FEAT] 견적요청서 우측 입력란 및 신청하기 버튼 추가

---
+++ fe-ldk/src/Quotation/InputGroup.js
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+
+
+const Inputitem = styled.input`
+    margin-bottom:40px;
+    height:70px;
+    border-radius:10px;
+    font-size:20px;
+    font-weight: bold;
+`;
+
+const InputLabel = styled.label`
+    font-size:20px;
+    font-weight:bold;
+    color:gray;
+`;
+
+const InputContainer = styled.div`
+    padding:10%;
+    padding-top:5%;
+    padding-bottom:5%;
+`;
+
+const TextArea = styled.textarea`
+    height:280px;
+    margin-bottom:40px;
+    border-radius:10px;
+    border:2px solid black;
+    font-size:20px;
+    font-weight: bold;
+    &::-webkit-input-placeholder { 
+        text-align:center;
+        vertical-align:middle;
+        padding-top:120px;
+    }
+  `;
+
+const Button = styled.input`
+    width:100%;
+    height:120px;
+    background-color:#B76464;
+    color:white;
+    font-weight:bold;
+    font-size:40px;
+    border-radius:10px;
+`;
+
+const InputGroup = () => {
+    return (
+        <>
+            <InputContainer className='container'>
+                <form>
+
+                    {/* 1행 */}
+                    <div className='row'>
+                        {/* <InputLabel for="TITLE">ex) 큐티뽀짝 레터링케이꾸</InputLabel> */}
+                        <Inputitem placeholder='제목' className='col' id='TITLE'/>
+                    </div>
+                    {/* 2행 */}
+                    <div className='row'>
+                        {/* <InputLabel for="PRICE">ex) 오억</InputLabel> */}
+                        <Inputitem placeholder='가격' className='col me-3' />
+                        {/* <InputLabel for="QUANTITY">ex) 백만개</InputLabel> */}
+                        <Inputitem placeholder='수량' className='col' />
+                    </div>
+                    {/* 3행 */}
+                    <div className='row'>
+                        {/* <InputLabel for="ZIPCODE">ex) </InputLabel> */}
+                        <Inputitem placeholder='우편번호' className='col' />
+                    </div>
+                    {/* 4행 */}
+                    <div className='row'>
+                        {/* <InputLabel for="DETAILEDADDRESS">ex) </InputLabel> */}
+                        <Inputitem placeholder='상세주소' className='col' />
+                    </div>
+                    {/* 5행 */}
+                    <div className='row'>
+                        {/* <InputLabel for="EXPLANATION">ex) 생크림을 윗면 둘레로 윗면에 나루토와 사스케 싸움 그림과 가슴이 웅장해진다 텍스트</InputLabel> */}
+                        <TextArea placeholder='설명' className='col' />
+                    </div>
+
+                    <div className='row'>
+                        <Button type='submit' value='신청하기' className='col' ></Button>
+                    </div>
+                </form>
+            </InputContainer>
+        </>
+    );
+};
+
+export default InputGroup;
--- fe-ldk/src/Quotation/Quotation.js
+++ fe-ldk/src/Quotation/Quotation.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Carousel from './Carousel';
 import styled from 'styled-components';
+import InputGroup from './InputGroup';
 
 const StyledContainer = styled.div`
   display: flex;
   width:100%;
   height:100%;
+  background-color: #FFE9E9;
 `;
 
 const Quotation = () => {
     return (
         <>
             <StyledContainer>
-                <Carousel classname></Carousel>
-
+                <Carousel></Carousel>
+                <InputGroup></InputGroup>
             </StyledContainer>
         </>
     );
